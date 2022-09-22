012f1df47c5e1867aa31178a360ef1a636e8aa3f
purpurlu <laurazurbayeva@gmail.com>
2022-09-22 11:18:29 +0100

    create a function to get all users

---
+++ src/utils/getUserList.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import {IUser} from './interfaces';
+
+export async function getUserList(): Promise <IUser[]> {
+    const res = await axios.get(`http://localhost:4000/users`)
+    const listOfUsers: IUser[] = [];
+
+    res.data.forEach((item: any) => {
+        listOfUsers.push({
+            userID: item.user_id,
+            username: item.user_name,
+            isFaculty: item.is_faculty
+        })
+    })
+    // console.log(res.data)
+    return listOfUsers
+
+}
