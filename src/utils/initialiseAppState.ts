e64b329d9609214ee3c85308c95e11237b3bb416
purpurlu <laurazurbayeva@gmail.com>
2022-09-22 11:19:12 +0100

    create initial values for appState

---
+++ src/utils/initialiseAppState.ts
@@ -0,0 +1,28 @@
+import { IAppState } from "./interfaces";
+
+export function initialiseAppState():IAppState {
+    const nullSearchQuery = {
+        searchTerm: "",
+        tag: []
+    }
+    const nullAppState:IAppState = {
+        loggedInUser: null,
+        userList: [
+                // {
+                //   userID: 1,
+                //   username: "Laura",
+                //   isFaculty: false,
+                // },
+                // {
+                //   userID: 2,
+                //   username: "Laura2",
+                //   isFaculty: false,
+                // }
+        ],  //add user list
+        studyResources: [],
+        allResources: [],
+        searchAllResources: nullSearchQuery,
+        searchStudyResources: nullSearchQuery
+    }
+    return nullAppState
+}
