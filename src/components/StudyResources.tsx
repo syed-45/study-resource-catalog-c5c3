058e6b6bd2d4821fb48e96a5c82f49a58149508e
Sevgi <sevgi2308@gmail.com>
2022-10-03 15:04:22 +0300

    study resources page ready not connected to BE

--- src/components/StudyResources.tsx
+++ src/components/StudyResources.tsx
@@ -1,13 +1,27 @@
-import { IAppState } from "../utils/interfaces";
+import { useEffect, useState } from "react";
+import { getFavourites } from "../utils/getFavourites";
+import { IAppState, IResource } from "../utils/interfaces";
+import { ResourceCard } from "./ResourceCard";
 
 interface StudyResourcesProps {
   appState: IAppState;
   setAppState: React.Dispatch<React.SetStateAction<IAppState>>;
 }
 
-export function StudyResources({
-  appState,
-  setAppState,
-}: StudyResourcesProps): JSX.Element {
-  return <p>Study Resources Pages</p>;
+export function StudyResources({ appState }: StudyResourcesProps): JSX.Element {
+  const [faves, setFaves] = useState<IResource[]>([]);
+  useEffect(() => {
+    if (appState.loggedInUser !== null) {
+      getFavourites(appState.loggedInUser.userID).then((fetchedFaves) =>
+        setFaves((prev) => fetchedFaves)
+      );
+    }
+  }, [setFaves]);
+  return (
+    <>
+      {faves.map((resource) => (
+        <ResourceCard key={resource.resourceID} resource={resource} appState={appState} />
+      ))}
+    </>
+  );
 }
