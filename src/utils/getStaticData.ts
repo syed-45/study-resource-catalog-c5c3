9647e900cd87b116ff1b39333e39a6d58ca802f7
syed <user.email>
2022-10-03 16:16:28 +0100

    format

--- src/utils/getStaticData.ts
+++ src/utils/getStaticData.ts
@@ -8,31 +8,23 @@ import {
 import { BASEURL } from "./interfaces";
 
 export async function getTags(): Promise<Itag[]> {
-  const res = await axios.get(
-    `${BASEURL}/tablename/tags`
-  );
+  const res = await axios.get(`${BASEURL}/tablename/tags`);
   return res.data;
 }
 
 export async function getWeeks(): Promise<IWeek[]> {
-  const res = await axios.get(
-    `${BASEURL}/tablename/buildweeks`
-  );
+  const res = await axios.get(`${BASEURL}/tablename/buildweeks`);
   return res.data;
 }
 
 export async function getRecommendationOptions(): Promise<
   IRecommendationOption[]
 > {
-  const res = await axios.get(
-    `${BASEURL}/tablename/recommendations`
-  );
+  const res = await axios.get(`${BASEURL}/tablename/recommendations`);
   return res.data;
 }
 
 export async function getContentTypes(): Promise<IContentType[]> {
-  const res = await axios.get(
-    `${BASEURL}/tablename/contenttypes`
-  );
+  const res = await axios.get(`${BASEURL}/tablename/contenttypes`);
   return res.data;
 }
