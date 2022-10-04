52fa613c0152e32c48c4ef05cb21e4dd87f6d415
syed <user.email>
2022-10-04 11:40:03 +0100

    formatted

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,18 @@
 import { Container, Navbar } from "react-bootstrap";
 import { IAppState } from "../utils/interfaces";
 
-
 interface NavigationBarProps {
   appState: IAppState;
 }
 
-export function Footer({
-  appState,
-}: NavigationBarProps): JSX.Element {
+export function Footer({ appState }: NavigationBarProps): JSX.Element {
   return (
     <div>
       <Navbar bg="warning" variant="light">
         <Container>
-          <Navbar.Brand>
-            Made By Team C5C3
-          </Navbar.Brand>          
+          <Navbar.Brand>Made By Team C5C3</Navbar.Brand>
         </Container>
       </Navbar>
     </div>
   );
 }
-
